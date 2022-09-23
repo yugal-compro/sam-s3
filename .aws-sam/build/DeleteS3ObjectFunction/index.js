46b8a3910c2e38cf375ee69edc94feb1a4c630c5
Yugal Vashishth <yugal.vashishth@comprotechnologies.com>
2022-09-23 10:21:01 +0530

    2nd

--- .aws-sam/build/DeleteS3ObjectFunction/index.js
+++ .aws-sam/build/DeleteS3ObjectFunction/index.js
@@ -1,19 +1,20 @@
+
 const aws = require('aws-sdk');
 
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 
 exports.handler = async (event, context) => {
-   
-   console.log(event);
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    // Get the object from the event and show its content type
   
     const params = {
         Bucket: event.bucketName,
         Key: event.key,
-        Body: event.body
     };
     try {
-        const { Body } = await s3.putObject(params).promise();
+        const { Body } = await s3.deleteObject(params).promise();
       const bodyContents = Body.toString();
         console.log('CONTENT TYPE:', bodyContents);
         return bodyContents;
